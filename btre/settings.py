cbac62a89e4bdd29dd5084bc4568da84b75cf40b
Yingying.li2018 <yingying.li2018@gmail.com>
2019-10-12 19:02:38 +0200

    remove email from local settings

--- btre/settings.py
+++ btre/settings.py
@@ -143,12 +143,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='yingying.zion@gmail.com'
-EMAIL_HOST_PASSWORD='50291733Yy'
-EMAIL_USE_TLS=True
+
 
 # Local settings Setup
 try:
